c4636ced29d26197ba21746f2249ebece3b2b724
Guillermo <guillermoebertola@gmail.com>
2025-05-27 23:49:04 +0000

    no se que decirte

--- toolkit.sh
+++ toolkit.sh
@@ -3,6 +3,19 @@
 
 ruta=""
 
+opcion1(){
+	find "$ruta" -maxdepth 1 -type f | wc -l
+	find "$ruta" -mindepth 2 -type f | wc -l
+
+
+}
+
+opcion5(){
+	whoami
+	uptime -p
+	date
+}
+
 opcion3(){
 #Muestra los permission denied
 	echo "Espacio en discos:"
@@ -17,7 +30,7 @@ opcion3(){
 ruta_predeterminada=""
 definir_ruta_predeterminada() {
     read -p "Ingresar ruta predeterminada: " ruta
-    if [[ -d "$ruta" ]]; then
+    if [[ -d "$ruta" ]]; then #No es necesario validar rutas
         ruta_predeterminada="$ruta"
         echo "Ruta predeterminada: $ruta_predeterminada"
     else
@@ -25,8 +38,8 @@ definir_ruta_predeterminada() {
     fi
 }
 
-renombrar_archivos() {
-   local ruta="$ruta_predeterminada"
+renombrar_archivos(){ 
+   local ruta="$ruta_predeterminada" #que es local
    if [ -z "$ruta" ]; then
         read -p "Introducir ruta del directorio: " ruta
     fi
@@ -34,52 +47,10 @@ if [ ! -d "$ruta" ]; then
         echo "Error: La ruta no es un directorio válido."
         return
     fi
-
-
-opcion7(){
-	echo "Ingrese ruta"
-	read ruta
-}
-
-opcion1(){
-	if [ $ruta -n ]; then
-	echo "Ingrese una ruta"
-	read ruta2
-	find "$ruta2" -maxdepth 1 -type f | wc -l #verificar que devuelve el numero correct
-	find "$ruta2" -mindepth 2 -type f | wc -l
-	find "$ruta2" -type f -printf "%s %f\n" > auxiliar1.txt
-        sort -nr auxiliar1.txt > auxiliar2.txt
-        head -n2 auxiliar2.txt > auxiliar1.txt
-        echo "Archivo mas pesado"
-        tail -n1 auxiliar1.txt
-        rm auxiliar1.txt auxiliar2.txt
-	find "$ruta2" -type f -printf "%s %f\n" > auxiliar1.txt
-        sort -n auxiliar1.txt > auxiliar2.txt
-        head -n2 auxiliar2.txt > auxiliar1.txt
-        echo "Archivo mas ligero"
-        tail -n1 auxiliar1.txt
-        rm auxiliar1.txt auxiliar2.txt
-
-	elif
-	find "$ruta" -maxdepth 1 -type f | wc -l
-	find "$ruta" -mindepth 1 -type f | wc -l
-	find "$ruta" -type f -printf "%s %f\n" > auxiliar1.txt
-        sort -nr auxiliar1.txt > auxiliar2.txt
-        head -n2 auxiliar2.txt > auxiliar1.txt
-        echo "Archivo mas pesado"
-        tail -n1 auxiliar1.txt
-        rm auxiliar1.txt auxiliar2.txt
-	find "$ruta" -type f -printf "%s %f\n" > auxiliar1.txt
-        sort -n auxiliar1.txt > auxiliar2.txt
-        head -n2 auxiliar2.txt > auxiliar1.txt
-        echo "Archivo mas ligero"
-        tail -n1 auxiliar1.txt
-        rm auxiliar1.txt auxiliar2.txt
-
 for archivo in "$ruta"/*; do
         if [ -f "$archivo" ]; then
             nuevo_nombre="${archivo}bckp"
-            if mv "$archivo" "$nuevo_nombre"; then 
+            if mv "$archivo" "$nuevo_nombre"; then #como funciona este if 
             	echo "Renombrado: $(basename "$archivo") -> $(basename "$nuevo_nombre")"
 	    else
 		echo "Error renombrando : $(basename "$archivo")"
@@ -127,7 +98,7 @@ guardar_pagina_web() {
     archivo="$carpeta/paginaweb.txt"
 
     echo "Descargando contenido..."
-    if curl -s "$url" -o "$archivo"; then
+    if curl -s "$url" -o "$archivo"; then #explicame plis
 	echo "Contenido guardado en: $archivo"
     else
 	echo "Error guardando la página."
